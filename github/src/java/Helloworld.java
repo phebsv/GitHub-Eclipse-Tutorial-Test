47a4030b0a10deef4a22924bf3f9954e81b8b005
oopa38 <oopa38@CL738.snjose.local>
2025-04-21 13:56:09 +0800

    third commit

--- github/src/java/Helloworld.java
+++ github/src/java/Helloworld.java
@@ -5,7 +5,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		
-		System.out.println("Hello Shipooo");
+		System.out.println("Hello Universe");
 
 	}
 
